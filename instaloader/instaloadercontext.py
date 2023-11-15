56e879f3fd8cdf3e22496e1ecafd26e7b52acf92
Benjamin Bialy <benjamincbialy@gmail.com>
2023-11-15 15:12:20 +1100

    Removed SSL verification on all get_json() http/s requests

--- instaloader/instaloadercontext.py
+++ instaloader/instaloadercontext.py
@@ -369,7 +369,7 @@ class InstaloaderContext:
                 self._rate_controller.wait_before_query('iphone')
             if is_other_query:
                 self._rate_controller.wait_before_query('other')
-            resp = sess.get('https://{0}/{1}'.format(host, path), params=params, allow_redirects=False)
+            resp = sess.get('https://{0}/{1}'.format(host, path), params=params, allow_redirects=False, verify=False)
             if resp.status_code in self.fatal_status_codes:
                 redirect = " redirect to {}".format(resp.headers['location']) if 'location' in resp.headers else ""
                 body = ""
@@ -389,7 +389,7 @@ class InstaloaderContext:
                                                  "some time, recreate the session and try again")
                 if redirect_url.startswith('https://{}/'.format(host)):
                     resp = sess.get(redirect_url if redirect_url.endswith('/') else redirect_url + '/',
-                                    params=params, allow_redirects=False)
+                                    params=params, allow_redirects=False, verify=False)
                 else:
                     break
             if response_headers is not None:
